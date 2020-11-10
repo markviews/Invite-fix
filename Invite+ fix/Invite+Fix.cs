d1243ae9efa7c8d81188d8c69bec26fcd09ef880
MarkViews <46404728+markviews@users.noreply.github.com>
2020-11-10 13:00:09 -0800

    Add files via upload
    upload

---
+++ Invite+ fix/Invite+Fix.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.Core;
+
+namespace Invite__fix {
+    public class Invite_fix : MelonMod {
+
+        GameObject menu;
+
+        public override void OnLevelWasInitialized(int level) {
+            menu = GameObject.Find("/UserInterface/MenuContent/Screens/UserInfo/User Panel/OnlineFriend");
+        }
+
+        public override void OnGUI() {
+            if (menu == null) return;
+            if (!menu.active) return;
+                if (RoomManager.field_Internal_Static_ApiWorldInstance_0?.InstanceType == ApiWorldInstance.AccessType.InvitePlus) {
+                    Button button = GameObject.Find("/UserInterface/MenuContent/Screens/UserInfo/User Panel/OnlineFriend/Actions/Invite").GetComponent<Button>();
+                    if (button.interactable) return;
+                        button.interactable = true;
+                        button.m_Interactable = true;
+                }
+        }
+
+    }
+}
