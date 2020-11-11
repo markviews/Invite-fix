7ffe494c7df32712881aae0b7ff2b093f76cb9b0
MarkViews <46404728+markviews@users.noreply.github.com>
2020-11-11 09:21:13 -0800

    Add files via upload
    optimized code a bit

--- Invite+ fix/Invite+Fix.cs
+++ Invite+ fix/Invite+Fix.cs
@@ -7,21 +7,21 @@ namespace Invite__fix {
     public class Invite_fix : MelonMod {
 
         GameObject menu;
+        Button button;
 
-        public override void OnLevelWasInitialized(int level) {
+        public override void VRChat_OnUiManagerInit() {
             menu = GameObject.Find("/UserInterface/MenuContent/Screens/UserInfo/User Panel/OnlineFriend");
+            button = GameObject.Find("/UserInterface/MenuContent/Screens/UserInfo/User Panel/OnlineFriend/Actions/Invite").GetComponent<Button>();
         }
 
         public override void OnGUI() {
             if (menu == null) return;
             if (!menu.active) return;
                 if (RoomManager.field_Internal_Static_ApiWorldInstance_0?.InstanceType == ApiWorldInstance.AccessType.InvitePlus) {
-                    Button button = GameObject.Find("/UserInterface/MenuContent/Screens/UserInfo/User Panel/OnlineFriend/Actions/Invite").GetComponent<Button>();
                     if (button.interactable) return;
                         button.interactable = true;
                         button.m_Interactable = true;
-                }
+            }
         }
-
     }
 }
