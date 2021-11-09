0371fcc2e6d06c9c4787e02e51da3e1db891e8c7
MarkViews <markmark9788@gmail.com>
2021-11-08 22:48:25 -0800

    fix for vrc build 1149
    can't invite from quick menu yet, you gotta open the big menu

--- Invite+ fix/Invite+Fix.cs
+++ Invite+ fix/Invite+Fix.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using VRC.Core;
 
-[assembly: MelonInfo(typeof(Invite__fix.Invite_fix), "Invite+ fix", "1.3.1", "MarkViews")]
+[assembly: MelonInfo(typeof(Invite__fix.Invite_fix), "Invite+ fix", "1.3.3", "MarkViews")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace Invite__fix {
@@ -14,6 +14,7 @@ namespace Invite__fix {
     public class Invite_fix : MelonMod {
 
         GameObject socialMenu;
+        Image background;
 
         public override void OnApplicationStart() {
             MelonCoroutines.Start(UiManagerInitializer());
@@ -21,7 +22,10 @@ namespace Invite__fix {
 
             public IEnumerator UiManagerInitializer() {
             while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
+
             socialMenu = GameObject.Find("UserInterface/MenuContent/Screens/Social");
+            background = GameObject.Find("UserInterface/MenuContent/Backdrop/Backdrop/Background").GetComponent<Image>();
+
             GameObject userInfo = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo");
             Button button = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo/OnlineFriendButtons/Invite").GetComponent<Button>();
 
@@ -36,8 +40,16 @@ namespace Invite__fix {
         }
 
         public IEnumerator delayRun(Action action) {
+            yield return new WaitForSeconds(0.1f);
+
+            if (socialMenu.active)
             while (socialMenu.active == true)
                 yield return null;
+
+            if (background.color.a < 0.9)
+                while (background.color.a < 0.9)
+                    yield return null;
+
             action.Invoke();
         }
 
