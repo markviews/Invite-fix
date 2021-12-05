2628f1ba635ad6644acb902deccf0612e4299fca
MarkViews <markmark9788@gmail.com>
2021-12-04 17:22:32 -0800

    fixed for vrc build 1156

--- Invite+ fix/Invite+Fix.cs
+++ Invite+ fix/Invite+Fix.cs
@@ -1,20 +1,17 @@
 ﻿using MelonLoader;
 using System;
 using System.Collections;
-using UIExpansionKit.Components;
+using UIExpansionKit.API;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC.Core;
 
-[assembly: MelonInfo(typeof(Invite__fix.Invite_fix), "Invite+ fix", "1.3.3", "MarkViews")]
+[assembly: MelonInfo(typeof(Invite__fix.Invite_fix), "Invite+ fix", "1.3.4", "MarkViews")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace Invite__fix {
 
     public class Invite_fix : MelonMod {
-
-        GameObject socialMenu;
-        Image background;
 
         public override void OnApplicationStart() {
             MelonCoroutines.Start(UiManagerInitializer());
@@ -23,34 +20,16 @@ namespace Invite__fix {
             public IEnumerator UiManagerInitializer() {
             while (VRCUiManager.prop_VRCUiManager_0 == null) yield return null;
 
-            socialMenu = GameObject.Find("UserInterface/MenuContent/Screens/Social");
-            background = GameObject.Find("UserInterface/MenuContent/Backdrop/Backdrop/Background").GetComponent<Image>();
-
-            GameObject userInfo = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo");
             Button button = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo/OnlineFriendButtons/Invite").GetComponent<Button>();
 
-            userInfo.AddComponent<EnableDisableListener>().OnEnabled += () => {
-                MelonCoroutines.Start(delayRun(() => {
-                    if (RoomManager.field_Internal_Static_ApiWorldInstance_0?.type == InstanceAccessType.InvitePlus) {
-                        button.interactable = true;
-                        button.m_Interactable = true;
-                    }
-                }));
-            };
-        }
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.UserDetailsMenu).AddSimpleButton("Invite", new Action(() => {
+                if (RoomManager.field_Internal_Static_ApiWorldInstance_0?.type == InstanceAccessType.InvitePlus) {
+                    button.onClick.Invoke();
+                } else {
+                    MelonLogger.Warning("UIX Invite button only works in Invite+ worlds");
+                }
+            }));
 
-        public IEnumerator delayRun(Action action) {
-            yield return new WaitForSeconds(0.1f);
-
-            if (socialMenu.active)
-            while (socialMenu.active == true)
-                yield return null;
-
-            if (background.color.a < 0.9)
-                while (background.color.a < 0.9)
-                    yield return null;
-
-            action.Invoke();
         }
 
     }
